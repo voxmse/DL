575bf33fcb4bafada44c21e9197d4d34825fbc39
Mykhaylo SEREDA <mser@softserveinc.com>
2022-01-11 22:10:20 +0200

    Decoding using SQL

--- sql/select_structured.demo.sql
+++ sql/select_structured.demo.sql
@@ -1,7 +1,3 @@
-DECLARE var_source_name STRING DEFAULT 'TEST';
-DECLARE var_path_data ARRAY<STRUCT<path STRING, types STRING>>;
-DECLARE _Z BYTES;
-
 -- unified source raw data structure. Normally is stored as a persistent table
 CREATE TEMPORARY TABLE source_data AS
 SELECT
@@ -22,7 +18,7 @@ SELECT
 ;
 
 -- paths to extract column data. Normally is stored as a persistent table
-CREATE TEMPORARY TABLE data_path AS
+CREATE TEMPORARY TABLE path_data AS
 SELECT
     'TEST' source_name,
     'DEEP' path,
@@ -63,33 +59,6 @@ SELECT
 --     'WRONG_TYPE' path,
 --     'q' types
 ;
-
-
--- array of data_path elements ordered by data_path.path
--- fails if col_path.name is not unique or a type is wrong. Normally is created as a table function
-SET var_path_data = (
-  SELECT
-    ARRAY_AGG(STRUCT(path, types) ORDER BY path) AS path_data
-  FROM (
-    SELECT
-      path,
-      types,
-      1 / CASE WHEN COUNT(*)OVER(PARTITION BY path)>1 OR EXISTS (SELECT t FROM UNNEST(SPLIT(types, ''))t WHERE t NOT IN('a','b','d','e','i','n','o','p','s','t','x','z')) THEN 0 ELSE 1 END AS tst
-    FROM
-      data_path
-    WHERE
-      source_name='TEST'  
-  )t
-  HAVING SUM(tst)>0
-);
-
--- string of concatenated ',' + data_path.path + ':' + ROW_NUMBER() + ',' values in the same order as elements of the above-mentioned array
-SET _Z = (
-  SELECT 
-     CAST(CONCAT(',', STRING_AGG(CONCAT(t.path, ':', CAST(offset AS STRING)) ORDER BY t.path), ',') AS BYTES)
-  FROM
-      UNNEST(var_path_data) AS t WITH OFFSET
-);
 
 
 
@@ -161,20 +130,33 @@ CREATE TEMP FUNCTION parseRow(jsonData STRING, pathData ARRAY<STRUCT<path STRING
     return resarr;
 """;
 
+CREATE TEMP FUNCTION getString(target_path STRING, path_arr ARRAY<STRUCT<path STRING, types STRING>>, data_arr ARRAY<STRUCT<type STRING, value STRING>>, default_value STRING) RETURNS STRING AS
+  ((WITH guess AS(SELECT ((SELECT offset idx FROM UNNEST(path_arr)t WITH OFFSET WHERE t.path=target_path)) AS idx)
+    SELECT CASE WHEN idx IS NULL OR data_arr[OFFSET(idx)].type<>'s' THEN default_value ELSE data_arr[OFFSET(idx)].value END FROM guess));     
 
-WITH
-    parsed AS(
-        SELECT
-            
+CREATE TEMP FUNCTION getDate(target_path STRING, path_arr ARRAY<STRUCT<path STRING, types STRING>>, data_arr ARRAY<STRUCT<type STRING, value STRING>>, default_value DATE) RETURNS DATE AS
+  ((WITH guess AS(SELECT ((SELECT offset idx FROM UNNEST(path_arr)t WITH OFFSET WHERE t.path=target_path)) AS idx)
+    SELECT CASE WHEN idx IS NULL OR data_arr[OFFSET(idx)].type<>'d' THEN default_value ELSE CAST(data_arr[OFFSET(idx)].value AS DATE) END FROM guess));  
 
 
-parseRow(
-    '{"ADDRESS:s:200":"221b, Baker Street, London, NW1 6XE, UK","BIRTHDAY:d":"2020-01-03","CHILDREN:s":null,"COMPLEX":{"FILEBODY:x":"s4pQ","FILENAME:s":"FILE1.TXT"},"DEEP":[{"A:i":null,"B:i":12},{"C:i":31}],"ID:i":1,"IS_MARRIED:b":false,"NAME:s":"Sherlock Holmes","SALARY:n:16:2":"12.45","TAXES:n":"687192025652473624789243787872498713367.89012","TEMPERATURE":{"INDOOR:e":21.8,"OUTDOOR:e":2.3},"VISIT_TS:z":"2022-01-07 21:51:57.205231"}',
-    [STRUCT('DEEP' AS path, 'a' AS types), STRUCT('BIRTHDAY' AS path, 'd' AS types), STRUCT('IS_MARRIED' AS path, 'b' AS types), STRUCT('COMPLEX.FILEBODY' AS path, 'x' AS types), STRUCT('COMPLEX.FILENAME' AS path, 's' AS types), STRUCT('CHILDREN' AS path, 's' AS types)]
-);
-
-WITH raw AS (
-  SELECT '{"ADDRESS:s:200":"221b, Baker Street, London, NW1 6XE, UK","BIRTHDAY:d":"2020-01-03","CHILDREN":[],"DEEP":[{"A:i":11,"B:i":12},{"C:i":31}],"ID:i":1,"IS_MARRIED:b":false,"NAME:s":"Sherlock Holmes","SALARY:n:16:2":"12.45","TAXES:n":"687192025652473624789243787872498713367.89012","TEMPERATURE":{"INDOOR:e":21.8,"OUTDOOR:e":2.3},"VISIT_TS:z":"2022-01-05 21:48:59.080790"}' row_data
-  UNION ALL
-  SELECT '{"ADDRESS:s":null,"BIRTHDAY:d":"1987-12-16","CHILDREN":[{":s":"John Watson Jr"}],"DEEP":[{"A:i":1,"B:i":2},{"A:i":3,"B:i":4}],"ID:i":1,"NAME:s":"John H. Watson","SALARY:n:16:2":"1245.12","TAXES:s":"0.45","TEMPERATURE":{"INDOOR:e":21.8,"OUTDOOR:e":2.3},"VISIT_TS:z":null}'
+WITH path_data AS(
+  SELECT
+    ARRAY_AGG(STRUCT(path, types) ORDER BY path) AS path_arr
+  FROM 
+    path_data
+  WHERE
+    source_name='TEST'
+), parsed_data AS (
+  SELECT
+    source_data.* EXCEPT(sys_data),
+    parseRow(sys_data, path_arr) parsed_arr
+  FROM
+    source_data,
+    path_data
 )
+SELECT
+  parsed_data.* EXCEPT(parsed_arr),
+  getString('COMPLEX.FILENAME', path_arr, parsed_arr, NULL) AS filename
+FROM
+  parsed_data,
+  path_data;
